5faaddee2b8efdc0c52e4be4a82402f45ba49fa2
sunxg <sunxg@fenbi.com>
2019-11-14 21:17:43 +0800

    ADD: FixedRouteHeader component

---
+++ src/components/FixedRouteHeader.vue
@@ -0,0 +1,167 @@
+<template>
+    <header>
+        <div class="sticky-wrapper" id="nav-sticky-wrapper">
+            <nav id="nav">
+                <div class="container">
+                    <a href="#" class="navbar-brand">
+                        <CoupleNames size="small" />
+                    </a>
+                    <div class="navbar-collapse">
+                        <ul id="nav-list">
+                            <li
+                                v-for="item in routeNameList"
+                                :key="item.name"
+                                :class="{ active: activeRoute === item.name }"
+                            >
+                                <a :href="item.anchor">{{ item.name }}</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import CoupleNames from './CoupleNames.vue';
+
+@Component({
+    components: {
+        CoupleNames
+    }
+})
+export default class FixedRouteHeader extends Vue {
+    activeRoute: string = 'Home';
+    routeNameList: any[] = [
+        {
+            name: 'Home',
+            anchor: '#home'
+        },
+        {
+            name: 'Couple',
+            anchor: '#couple'
+        },
+        {
+            name: 'Our Sotry',
+            anchor: '#our_story'
+        },
+        {
+            name: 'Gallery',
+            anchor: '#gallery'
+        },
+        {
+            name: 'Featured People',
+            anchor: '#featured_people'
+        },
+        {
+            name: 'RSVP',
+            anchor: '#rsvp'
+        }
+    ];
+}
+</script>
+<style scoped lang="less">
+.sticky-wrapper {
+    width: 100%;
+    height: 50px;
+
+    #nav {
+        font-family: 'Oswald', Verdana, Geneva, sans-serif;
+        font-size: 20px;
+        z-index: 998;
+        width: 100%;
+        padding: 20px 0px;
+        background-color: #fff;
+        border: 0px;
+        border-radius: 4px;
+        position: relative;
+        min-height: 50px;
+        margin-bottom: 20px;
+
+        .container {
+            margin-right: auto;
+            margin-left: auto;
+            width: 1000px;
+            height: 50px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+
+            .navbar-brand {
+                float: left;
+                padding: 15px 15px;
+                font-size: 18px;
+                line-height: 20px;
+                color: #777;
+                margin-left: -60px;
+            }
+
+            .navbar-collapse {
+                padding-right: 15px;
+                padding-left: 15px;
+                overflow-x: visible;
+                -webkit-overflow-scrolling: touch;
+                display: block !important;
+                height: auto !important;
+                padding-bottom: 0;
+                width: auto;
+                border-top: 0;
+                -webkit-box-shadow: none;
+                box-shadow: none;
+                height: 50px;
+
+                &::after {
+                    clear: both;
+                }
+
+                #nav-list {
+                    padding: 0px 30px;
+                    float: right !important;
+                    margin-right: -150px;
+                    list-style: none;
+
+                    li {
+                        position: relative;
+                        display: block;
+                        float: left;
+
+                        a {
+                            color: #777;
+                            padding: 10px 6px;
+                            line-height: 20px;
+                            position: relative;
+                            display: block;
+                            text-decoration: none;
+                            background-color: transparent;
+                            font-weight: normal;
+
+                            &::after {
+                                content: '/';
+                                padding-left: 10px;
+                                color: rgba(0, 0, 0, 0.2);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+.circle {
+    margin: 0 20px;
+    background: #f9667e;
+    width: 100px;
+    height: 100px;
+    text-align: center;
+    font-size: 63px;
+    font-weight: normal;
+    line-height: 1.5em;
+    border-radius: 50%;
+    color: #fff;
+    top: -15px;
+    position: relative;
+}
+</style>
