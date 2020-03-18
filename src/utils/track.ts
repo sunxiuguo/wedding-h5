4b1f7e1affef50ddd20595f7541247f456f998b5
sunxg <sunxg@fenbi.com>
2020-03-18 19:29:11 +0800

    ADD: aliyun log track

---
+++ src/utils/track.ts
@@ -0,0 +1,54 @@
+import queryString from 'query-string';
+import { v4 as uuid } from 'uuid';
+
+let image: HTMLImageElement | null;
+
+export class BaseTrack {
+    private static serverUrl: string =
+        'https://zhusun.cn-beijing.log.aliyuncs.com/logstores/zhusun-online/track_ua.gif?APIVersion=0.6.0&';
+
+    public static track() {
+        try {
+            const qs = queryString.stringify({
+                timestamp: Date.now(),
+                traceId: this.getTraceId()
+            });
+            this.reportByImg(qs);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    private static reportByImg(qs: string, retryTimes: number = 3) {
+        const retry = () => {
+            image = null;
+            retryTimes && this.reportByImg(qs, retryTimes - 1);
+        };
+        return new Promise(resolve => {
+            try {
+                image = new Image();
+                image.src = this.serverUrl + qs;
+
+                image.onerror = () => {
+                    retry();
+                };
+            } catch (e) {
+                console.error(e);
+            }
+        });
+    }
+
+    private static getTraceId() {
+        try {
+            const traceKey = 'yfd_track_id';
+            let traceId = localStorage.getItem(traceKey);
+            if (!traceId) {
+                traceId = uuid();
+                localStorage.setItem(traceKey, traceId!);
+            }
+            return traceId;
+        } catch (e) {
+            return '';
+        }
+    }
+}
