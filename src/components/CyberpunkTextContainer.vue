82ac9a32f3698544e73cce53b958070c176b8e02
sunxg <sunxg@fenbi.com>
2021-01-27 16:53:26 +0800

    MOD: 适配vw

--- src/components/CyberpunkTextContainer.vue
+++ src/components/CyberpunkTextContainer.vue
@@ -14,21 +14,22 @@ export default class CyberpunkTextContainer extends Vue {
 }
 </script>
 <style scoped lang="less">
+@base: 1200;
 .text-magic {
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%) scale(2.4);
-    width: 300px;
-    font-size: 36px;
+    width: (300 / @base) * 100vw;
+    font-size: (36 / @base) * 100vw;
     font-family: Raleway, Verdana, Arial;
     color: #fff;
 }
 .white {
     position: absolute;
-    left: -10px;
+    left: -(10 / @base) * 100vw;
     width: 100%;
-    height: 3px;
+    height: (3 / @base) * 100vw;
     background: #000;
     z-index: 4;
     animation: whiteMove 3s ease-out infinite;
@@ -52,69 +53,69 @@ export default class CyberpunkTextContainer extends Vue {
     content: attr(data-word);
     position: absolute;
     top: 0;
-    left: -3px;
-    height: 36px;
+    left: -(3 / @base) * 100vw;
+    height: (36 / @base) * 100vw;
     color: rgba(255, 255, 255, 0.8);
     overflow: hidden;
     z-index: 3;
     background: rgba(0, 0, 0, 0.9);
     animation: redHeight 1.5s ease-out infinite;
     filter: contrast(200%);
-    text-shadow: -1px 0 0 cyan;
+    text-shadow: -(1 / @base) * 100vw 0 0 cyan;
     mix-blend-mode: darken;
 }
 
 @keyframes redShadow {
     20% {
-        height: 32px;
+        height: (32 / @base) * 100vw;
     }
     60% {
-        height: 6px;
+        height: (6 / @base) * 100vw;
     }
     100% {
-        height: 42px;
+        height: (42 / @base) * 100vw;
     }
 }
 
 @keyframes redHeight {
     20% {
-        height: 42px;
+        height: (42 / @base) * 100vw;
     }
     35% {
-        height: 12px;
+        height: (12 / @base) * 100vw;
     }
     50% {
-        height: 40px;
+        height: (40 / @base) * 100vw;
     }
     60% {
-        height: 20px;
+        height: (20 / @base) * 100vw;
     }
     70% {
-        height: 34px;
+        height: (34 / @base) * 100vw;
     }
     80% {
-        height: 22px;
+        height: (22 / @base) * 100vw;
     }
     100% {
-        height: 0px;
+        height: (0 / @base) * 100vw;
     }
 }
 
 @keyframes whiteMove {
     8% {
-        top: 38px;
+        top: (38 / @base) * 100vw;
     }
     14% {
-        top: 8px;
+        top: (8 / @base) * 100vw;
     }
     20% {
-        top: 42px;
+        top: (42 / @base) * 100vw;
     }
     32% {
-        top: 2px;
+        top: (2 / @base) * 100vw;
     }
     99% {
-        top: 30px;
+        top: (30 / @base) * 100vw;
     }
 }
 </style>
